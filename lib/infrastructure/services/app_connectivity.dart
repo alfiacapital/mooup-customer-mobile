4c0b88dfda036c5f9ceb72e23bd59df036697a79
alfia <tech.alfiasystem@gmail.com>
2025-10-29 15:26:30 +0100

    fckbhndesijfghiuedgh

--- lib/infrastructure/services/app_connectivity.dart
+++ lib/infrastructure/services/app_connectivity.dart
@@ -1,15 +1,27 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/widgets.dart';
 
 abstract class AppConnectivity {
   AppConnectivity._();
 
   static Future<bool> connectivity() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult.contains(ConnectivityResult.mobile) ||
-        connectivityResult.contains(ConnectivityResult.ethernet) ||
-        connectivityResult.contains(ConnectivityResult.wifi)) {
+    try {
+      // Defer to avoid iOS 18 launch-time plugin crash
+      if (WidgetsBinding.instance.lifecycleState == null) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {});
+        await Future<void>.delayed(const Duration(milliseconds: 300));
+      }
+
+      final connectivityResult = await Connectivity().checkConnectivity();
+      if (connectivityResult.contains(ConnectivityResult.mobile) ||
+          connectivityResult.contains(ConnectivityResult.ethernet) ||
+          connectivityResult.contains(ConnectivityResult.wifi)) {
+        return true;
+      }
+      return false;
+    } catch (_) {
+      // Fail open at startup instead of crashing
       return true;
     }
-    return false;
   }
 }
